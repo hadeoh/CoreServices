f1d8af6df59721c51749ada43dd8144463c8d4ed
Usman Adio <usthmandanfodio@gmail.com>
2021-09-08 08:55:23 +0100

    Fix error fro refresh wallet endpoint

--- Question 2 - Creating Logic.js
+++ Question 2 - Creating Logic.js
@@ -3,12 +3,13 @@ const { default: axios } = require('axios');
 async function getUserInfo(username, password) {
     const user = await makePostRequest('https://api.okra.ng/v2/mock-api/login', {username, password})
     const userProfile = user.data.data.profile
-    // const userRefereshWallet = await makePostRequest('https://api.okra.ng/v2/mock-api/refresh-wallet', {wallet_id: userProfile.id, variable: 'variable'})
-    // userRefereshWallet = userRefereshWallet.data
+
+    let userRefereshWallet = await makePostRequest('https://api.okra.ng/v2/mock-api/refresh-wallet', {id: userProfile.id, variable: 'variable'})
+    userRefereshWallet = userRefereshWallet.data.data.wallet.amount.toString()
     let logout = await axios.get('https://api.okra.ng/v2/mock-api/logout')
     logout = logout.data.message
     console.log('user_name: ', userProfile.name, '\nuser_id: ', 
-    userProfile.id, '\nuser_wallet_amount_before: ', userProfile.wallet.amount.toString(), '\nuser_wallet_amount_after: ', 
+    userProfile.id, '\nuser_wallet_amount_before: ', userProfile.wallet.amount.toString(), '\nuser_wallet_amount_after: ', userRefereshWallet,
     '\nlogout_message: ', logout);
 }
 
